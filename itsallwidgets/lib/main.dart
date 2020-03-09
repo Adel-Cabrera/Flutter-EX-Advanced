3b6a2f866542de9a0d56c9ae0bf7deae7568047e
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-03-09 13:20:27 -0300

    Creating listView builder and HomePage components

--- itsallwidgets/lib/main.dart
+++ itsallwidgets/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:itsallwidgets/src/pages/home_temp.dart';
+
+import 'src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomePageTemp(),
+      home: HomePage(),
     );
   }
 }
