c9bba6cd5ad2db0698a8a8213ac3d4c52c571c52
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-03-11 15:23:15 -0300

    finished input_page & added localization options for built-in widgets

--- itsallwidgets/lib/main.dart
+++ itsallwidgets/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:itsallwidgets/src/routes/routes.dart';
 
 import 'src/pages/alert_page.dart';
@@ -10,6 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('es', 'ES'),
+      ],
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: HomePage(),
--- itsallwidgets/lib/src/routes/routes.dart
+++ itsallwidgets/lib/src/routes/routes.dart
@@ -3,6 +3,7 @@ import 'package:itsallwidgets/src/pages/alert_page.dart';
 import 'package:itsallwidgets/src/pages/animated_container.dart';
 import 'package:itsallwidgets/src/pages/avatar_page.dart';
 import 'package:itsallwidgets/src/pages/card_page.dart';
+import 'package:itsallwidgets/src/pages/input_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
@@ -11,5 +12,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+    'inputs': (BuildContext context) => InputPage(),
   };
 }
